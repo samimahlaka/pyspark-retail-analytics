9af780369d8d2e556e4dc21e82ac5ea2b6f0b940
Mahlaka <samimahlaka@gmail.com>
2026-06-09 11:33:20 -0500

    Add revenue by city analysis

--- scripts/transform_data.py
+++ scripts/transform_data.py
@@ -1,15 +1,20 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, sum 
 
 
-spark =  SparkSession.builder.appName('RetaiL Transform').master('local[*]').config('spark.shuffle.partitions',"4"x``).getOrCreate()
+spark =  SparkSession.builder.appName('RetaiL Transform').master('local[*]').config('spark.shuffle.partitions',"4").getOrCreate()
 
 df = spark.read.csv('data/Retail_Transactions_Dataset.csv', header=True, inferSchema=True)
 
 clean_df = df.dropna().filter(col('Total_Cost') > 0).cache()
 
+city_revenue = clean_df.groupBy("City").agg(sum("Total_Cost").alias("Revenue")).orderBy(col("Revenue").desc())
+
 print(f'Original Rows : {df.count()}')
 print(f'Cleaned Rows : {clean_df.count()}')
+
+print(f'Total Cities : {city_revenue.count()}')
+city_revenue.show(10)
 
 clean_df.write.mode('overwrite').parquet('output/retail_transactions_cleaned')
 
