b545ba39633efb990a4939eda460394a98f4f8b5
Mahlaka <samimahlaka@gmail.com>
2026-06-09 19:58:15 -0500

    Add product revenue analysis

--- scripts/transform_data.py
+++ scripts/transform_data.py
@@ -2,37 +2,62 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, sum 
 
 
-spark =  SparkSession.builder.appName('RetaiL Transform').master('local[*]').config('spark.sql.shuffle.partitions',"4"
-                                                                                    ).getOrCreate()
+spark =  (SparkSession.builder.appName('RetaiL Transform')
+          .master('local[*]')
+          .config('spark.sql.shuffle.partitions',"4")
+          .getOrCreate())
 
 df = spark.read.csv('data/Retail_Transactions_Dataset.csv', header=True, inferSchema=True)
 
 clean_df = df.dropna().filter(col('Total_Cost') > 0).cache()
 
-city_revenue = clean_df.groupBy("City").agg(sum("Total_Cost").alias("Revenue")).orderBy(col("Revenue").desc())
+city_revenue = (
+                clean_df.groupBy("City")
+                .agg(sum("Total_Cost").alias("Revenue"))
+                .orderBy(col("Revenue").desc())
+                )
 
-store_revenue = clean_df.groupBy("Store_Type").agg(sum("Total_Cost").alias("Revenue")).orderBy(col("Revenue").desc())
+store_revenue = (
+                clean_df.groupBy("Store_Type")
+                .agg(sum("Total_Cost").alias("Revenue"))
+                .orderBy(col("Revenue").desc())
+                )
 
-cutomer_category_revenue = clean_df.groupBy("Customer").agg(sum("Total_Cost").alias("Revenue")).orderBy(col("Revenue").desc)
+customer_category_revenue = (
+                clean_df.groupBy("Customer_Category")
+                .agg(sum("Total_Cost").alias("Revenue"))
+                .orderBy(col("Revenue").desc())
+                )
 
 payment_revenue = (
-    clean_df
-    .groupBy("Payment_Method")
-    .agg(sum("Total_Cost").alias("Revenue"))
-    .orderBy(col("Revenue").desc())
-)
+                clean_df
+                .groupBy("Payment_Method")
+                .agg(sum("Total_Cost").alias("Revenue"))
+                .orderBy(col("Revenue").desc())
+                 )
 
-print(f'Original Rows : {df.count()}')
+
+product_revenue = (
+                    clean_df.groupBy("Product")
+                    .agg(sum("Total_Cost").alias("Revenue")) 
+                    .orderBy(col("Revenue").desc())
+                    )
+
+
+
+
+print(f'Total Original Rows : {df.count()}')
 print(f'Cleaned Rows : {clean_df.count()}')
-
 print(f'Total Cities : {city_revenue.count()}')
+print(f'Total Product Categories : {product_revenue.count()}')
+    
 city_revenue.show(10)
-
 store_revenue.show(20)
-cutomer_category_revenue.show(20)
+customer_category_revenue.show(20)
+product_revenue.show(20)
 
 
-# Revenue by Payment Method
+# Revenue by Paymen Method
 
 
 payment_revenue.show(20)
